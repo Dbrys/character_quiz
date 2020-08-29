a59dd82a053fe0af67c7f8df4a963c96616b5665
Dillon Brys <dillonbrys@dillons-mbp.home>
2020-08-28 23:50:21 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import Counter from './components/Counter';
 import './App.css';
 
+const headerStyle = {
+  fontSize: '100px',
+  fontFamily: 'Pacifico',
+};
+
 const App = () => (
-  <div className="flex min-h-screen font-sans bg-grey-200 items-center">
+  <div id="main" className="flex-col min-h-screen font-sans bg-grey-200 items-center">
     <div className="container mx-auto text-center">
-      <Counter />
+      <h1 style={headerStyle}>Cartoon Character Quiz</h1>
+    </div>
+    <div className="container mx-auto text-center">
+      <button type="button" className="bg-pink-400 text-white rounded py-2 px-4 mt-2">Start Quiz</button>
     </div>
   </div>
 );
+
 
 export default App;
